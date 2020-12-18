d9b59c87c6610ad4b5474a7163601c28dbb658c2
Dominik Jancik <hi@domj.net>
2020-12-18 14:37:31 +0100

    Added example post processing effects

--- os-library-spa-3d/src/shared/components/layout/BaseScene/BaseScene.tsx
+++ os-library-spa-3d/src/shared/components/layout/BaseScene/BaseScene.tsx
@@ -1,12 +1,18 @@
+import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
 import React, { FunctionComponent } from 'react'
-import { Box } from '../../Box/Box'
 
-export const BaseScene: FunctionComponent = ({children}) => {
+export const BaseScene: FunctionComponent = ({ children }) => {
     return (
         <>
+            {children}
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            {children}
+            <EffectComposer>
+                <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
+                <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+                <Noise opacity={0.02} />
+                <Vignette eskil={false} offset={0.1} darkness={1.1} />
+            </EffectComposer>
         </>
     )
 }
