fad662e10c0cca18fef97501f3b0c09a17787c51
Dominik Jancik <hi@domj.net>
2020-12-18 12:54:03 +0100

    Cleanup

--- os-library-spa/src/features/library/librarySlice.ts
+++ os-library-spa/src/features/library/librarySlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, ThunkDispatch } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AppThunk, RootState } from "../../app/store";
 import axios from '../../app/axios'
 
