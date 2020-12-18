23bb57b810df925d9a09dcded27bd08c0b1aacd4
Dominik Jancik <hi@domj.net>
2020-12-18 14:22:34 +0100

    Cleanup

--- os-library-spa-3d/src/App.tsx
+++ os-library-spa-3d/src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Canvas } from 'react-three-fiber';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Libraries } from './features/libraries/Libraries';
-import { LibraryEdit } from './features/library/LibraryEdit/LibraryEdit';
-import { Box } from './shared/components/Box/Box';
 import { BaseScene } from './shared/components/layout/BaseScene/BaseScene';
 
 function App() {
